21a6aac162cafcba51f45a103adb09c179171750
xingrui <xingrui@4paradigm.com>
2019-10-27 21:04:59 +0800

    fix: bugfix for extra string

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package gomodtest
 import "fmt"
 
 func Hi(name string, lang string, time string) string {
-	return fmt.Sprintf("Hi, %s![v2 version lang:%s time]", name, lang, time)
+	return fmt.Sprintf("Hi, %s![v2 version lang:%s time:%s]", name, lang, time)
 }
